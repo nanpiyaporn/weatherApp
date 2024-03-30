969d8109c77221848210297d19564f6971472ae7
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-30 02:08:41 -0400

    Refactor component name in forecast.jsx

--- weather_app/src/Components/forecast.jsx
+++ weather_app/src/Components/forecast.jsx
@@ -10,7 +10,7 @@ import "./Forecast.css";
 
 const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-const Forecast = ({ data }) => {
+const forecast = ({ data }) => {
   const dayInAWeek = new Date().getDay();
   const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
   
