45ff3a367776e3a1561685c1e1c69dec3c03b746
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-30 01:34:09 -0400

    Refactor API and App components

--- weather_app/src/Api.jsx
+++ weather_app/src/Api.jsx
@@ -1,17 +1,11 @@
-
-//const axios = require('axios');
-const API_KEY = import.meta.env.VITE_APP_API_KEY;
-
 export const geoApiOptions = {
-
-method: 'GET',
-	headers: {
-		'X-RapidAPI-Key':  API_KEY,
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-}
-
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-
-	
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
