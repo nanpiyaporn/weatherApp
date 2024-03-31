7ef103ab45a05c553aea04405bab4d4a83908808
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-31 16:45:56 -0400

    Add weather statistics and search box styling

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -9,14 +9,25 @@ const api = {
 function App() {
   const [search, setSearch] = useState("");
   const [weather, setWeather] = useState({});
+  const [weatherData, setWeatherData] = useState(null);
+
 
   /*
     Search button is pressed. Make a fetch call to the Open Weather Map API.
   */
+ // Calculate statistics
+let maxTemp;
+if (weatherData) {
+  const temperatures = weatherData.map(day => day.temp);
+  maxTemp = Math.max(...temperatures);
+ //minTemp = Math.min(...temperatures);
+  //avgTemp = temperatures.reduce((a, b) => a + b, 0) / temperatures.length;
+}
   const searchPressed = () => {
     fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
       .then((res) => res.json())
       .then((result) => {
+        console.log(weather);
         setWeather(result);
       });
   };
@@ -28,13 +39,18 @@ function App() {
         <h1>Weather App</h1>
 
         {/* Search Box - Input + Button  */}
-        <div>
+        <div  className ="inputBox">
+          <div className = "search-box">
           <input
             type="text"
             placeholder="Enter city/town..."
             onChange={(e) => setSearch(e.target.value)}
           />
+           <p></p>
+          </div>
+          <div className = "submit">
           <button onClick={searchPressed}>Search</button>
+          </div>
         </div>
 
         {/* If weather is not undefined display results from API */}
@@ -49,7 +65,14 @@ function App() {
             {/* Condition (Sunny ) */}
             <p>{weather.weather[0].main}</p>
             <p>({weather.weather[0].description})</p>
+            <p>{weatherData && (
+              <div className="dashboard">
+              <p>Max temperature: {maxTemp}</p>
+              
+            </div>
+      )}</p>
           </div>
+          
         ) : (
           ""
         )}
